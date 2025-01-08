f70980f484ec8baf27e69d23f75f8ccdecd178b8
Gwangho Kim <150674914+ghkimdev@users.noreply.github.com>
2025-01-08 10:59:17 +0900

    Update install_terraform.sh

--- install/install_terraform.sh
+++ install/install_terraform.sh
@@ -9,4 +9,5 @@ https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
 sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update && sudo apt-get install -y terraform
 
+sudo chown -R $(id -u):$(id -g) /opt
 cp -r /opt/devsecops-installation/install/terraform /opt/terraform
