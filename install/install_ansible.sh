6f7a16af0f2864b30e532d725df300e051c92701
Gwangho Kim <150674914+ghkimdev@users.noreply.github.com>
2025-01-08 10:58:49 +0900

    Update install_ansible.sh

--- install/install_ansible.sh
+++ install/install_ansible.sh
@@ -2,15 +2,17 @@
 
 ssh-keygen -t rsa -b 3072 -f ~/.ssh/id_rsa -N ""
 
+sudo apt update -qq
+sudo apt install -y python3 python3-pip
+
+pip3 install ansible boto3 botocore
+
 cat <<EOF | sudo tee -a ~/.bashrc
 export ANSIBLE_CONFIG=/opt/ansible/ansible.cfg
 EOF
 
 sudo chown -R $(id -u):$(id -g) /opt
-cp -r /opt/install/ansible /opt/ansible
+cp -r /opt/devsecops-installation/install/ansible /opt/ansible
 
-sudo apt update -qq
-sudo apt install -y python3 python3-pip
 
-pip3 install ansible boto3 botocore
 
